47aab875771f7285e0b07d72b8eb717a0d8e9656
mollybrace <molly.rose.brace@gmail.com>
2023-04-27 13:04:52 +0700

    adding div

--- src/lib/components/Testimonials.svelte
+++ src/lib/components/Testimonials.svelte
@@ -3,26 +3,24 @@
 
 
 <section id="testimonials"> 
-    <div class="max-w-6xl absolute px-5 mx-auto mt-32 text-center"></div>
-    <h2 class="text-4xl font-bold text-center">Testimonials</h2>
+    <div class="max-w-6xl relative px-5 mx-auto mt-10 text-center">
+    <h2 class="text-4xl font-bold text-center pb-0 text-darkPurple">Testimonials</h2>
 
     <div class="flex flex-col mt-24 md:flex-row md:space-x-6">
-        <div class="flex flex-col items-center p-6 m-6 space-y-6 rounded-lg bg-lightPurple md:w-1/3">
-            <p>image container:</p>
+           <div class="flex flex-col items-center p-6 m-6 space-y-6 rounded-lg bg-lightPurple md:w-1/3">
             <h5>Molly</h5>
             <p>"Oisin was bla bla bla"</p>
         </div>
 
         <div class="flex flex-col items-center p-6 m-6 space-y-6 rounded-lg bg-lightPurple md:w-1/3">
-            <p>image container:</p>
             <h5>Molly</h5>
             <p>"Oisin was bla bla bla"</p>
         </div>
 
         <div class="flex flex-col items-center p-6 m-6 space-y-6 rounded-lg bg-lightPurple md:w-1/3">
-            <p>image container:</p>
             <h5>Molly</h5>
             <p>"Oisin was bla bla bla"</p>
+        </div>
         </div>
     </div>
 
